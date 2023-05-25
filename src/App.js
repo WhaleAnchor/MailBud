77bc991cb42557f64fac0453062eeeaa8a684146
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-05-25 12:06:38 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Login from './components/Login'
+import Register from './components/Register';
+import Reset from './components/Reset';
 
 function App() {
   return (
