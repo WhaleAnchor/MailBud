565b7a9f9dc750465df57882d6e12c534ed0bf6a
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-05-25 16:10:03 -0700

    style change to navbar to make it look like a manila folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.css'
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/catlogo.png'; // import your logo image file
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo">
+      <NavLink to="/" className="logo">
         <img src={logo} alt="MailBud logo" />
         MailBud
-      </Link>
+      </NavLink>
       <div className="nav-links">
-        <Link to="/mail">Mail</Link>
-        <Link to="/packages">Packages</Link>
-        <Link to="/profiles">User Profiles</Link>
-        <Link to="/history">History</Link>
-        <Link to="/profile">Profile</Link> {/* This can be replaced with user's profile picture if available */}
+        <NavLink to="/mail" className={(navData) => (navData.isActive ? "active-tab" : 'none')}>Mail</NavLink>
+        <NavLink to="/packages" className={(navData) => (navData.isActive ? "active-tab" : 'none')}>Packages</NavLink>
+        <NavLink to="/profiles" className={(navData) => (navData.isActive ? "active-tab" : 'none')}>Profiles</NavLink>
+        <NavLink to="/history" className={(navData) => (navData.isActive ? "active-tab" : 'none')}>History</NavLink>
+        <NavLink to="/profile" className={(navData) => (navData.isActive ? "active-tab" : 'none')}>Profile</NavLink>
+        
       </div>
     </nav>
   );
