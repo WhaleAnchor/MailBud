c7834f7c189fba0f0895c4fd3e4a4b9224580e5e
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-05-25 16:08:49 -0700

    added new routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Container from './components/Container'
+import Navbar from './components/Navbar'
+
 import Login from './components/Login'
 import Register from './components/Register';
 import Reset from './components/Reset';
-import Mail from './components/Mail';
-import Container from './components/Container'
 
-import Navbar from './components/Navbar'
+import Mail from './components/Mail';
+import Packages from './components/Packages';
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
             <Route exact path="/register" element={<Register />} />
             <Route exact path="/reset" element={<Reset />} />
             <Route exact path="/mail" element={<Mail />} />
+            <Route exact path="/packages" element={<Packages />} />
           </>
         </Routes>
       </Container>
