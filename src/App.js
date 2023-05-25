9e325db7588cad216851c1682eab1a0792cd7bf3
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-05-25 15:12:42 -0700

    renamed components for clarity

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/Login'
 import Register from './components/Register';
 import Reset from './components/Reset';
-import Dashboard from './components/Dashboard';
+import Mail from './components/Mail';
+import Container from './components/Container'
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-        </>
+      <Navbar />
+      <Container>
+        <Routes>
+          <>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/reset" element={<Reset />} />
+            <Route exact path="/mail" element={<Mail />} />
+          </>
         </Routes>
+      </Container>
+      
     </Router>
   );
 }
