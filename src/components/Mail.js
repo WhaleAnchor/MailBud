731cb9d513ce9dc202215e9b167a66b377bc5a96
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-05-25 16:09:44 -0700

    style changes to tabs and added mailbox grids

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import './Mail.css'; // Don't forget to create this CSS file
+import { BsEnvelope } from 'react-icons/bs';
 
 function Mail() {
-    return (
-        <div>
-            hi
+  const mailboxes = Array.from({ length: 1000 }, (_, i) => i + 1);
+
+  return (
+    <div className="mail-grid">
+      {mailboxes.map((mailboxNumber) => (
+        <div key={mailboxNumber} className="mailbox">
+          <div className="mailbox-number">{mailboxNumber}</div>
+          <div className="mailbox-icon">
+            <BsEnvelope size={30} /> {/* Adjust the size as needed */}
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Mail;
